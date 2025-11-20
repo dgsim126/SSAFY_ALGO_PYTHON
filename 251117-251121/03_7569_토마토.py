912f238c4cd08725100e8fef80ed7171efe612dc
dgsim126 <dgsim126@gmail.com>
2025-11-20 11:43:56 +0900

    Initial commit

---
+++ 251117-251121/03_7569_토마토.py
@@ -0,0 +1,16 @@
+
+
+
+
+## main ##
+M, N, H= map(int, input().split()) # 가로, 세로, 높이
+box= [] # 토마토를 담은 곳
+
+for i in range(H):
+    floor= []
+    for j in range(N):
+        row= list(map(int, input().split()))
+        floor.append(row)
+    box.append(floor)
+
+solution()
